1cd0d14c83df9a0b8563f4f11d048464d1304856
duyhien2410 <hienduy2410@gmail.com>
2022-03-31 17:13:54 +0700

    up

--- C1221G1_TranLeDuyHien_Module3/src/ss4_cac_ham_thong_dung_trong_sql/bai_tap/luyen_tap_cac_ham_thong_dung_sql/luyen_tap_cac_ham_thong_dung_sql.sql
+++ C1221G1_TranLeDuyHien_Module3/src/ss4_cac_ham_thong_dung_trong_sql/bai_tap/luyen_tap_cac_ham_thong_dung_sql/luyen_tap_cac_ham_thong_dung_sql.sql
@@ -1,10 +1,12 @@
 use quan_ly_sinh_vien_ss3;
 
-select s.sub_id, s.sub_name, max(s.credit) as credit, s.`status`
-from `subject` s;
+select *
+from `subject` s
+where s.credit = (select max(credit) from `subject`);
 
-select s.sub_id, s.sub_name, max(m.mark) as mark_max, s.`status`
-from `subject` s inner join mark m on s.sub_id = m.sub_id;
+select s.sub_id, s.sub_name, m.mark, s.`status`
+from `subject` s inner join mark m on s.sub_id = m.sub_id
+where m.mark = (select max(mark) from mark);
 
 select s.student_id, s.student_name, s.`status`,s.class_id, avg(m.mark) as mark
 from student s inner join mark m on s.student_id = m.student_id
